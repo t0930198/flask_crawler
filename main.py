aa2fb761458f774f4b25f158d122e2825c97b09d
tony <t0930198@gmail.com>
2017-10-30 00:56:30 +0800

    init

---
+++ main.py
@@ -0,0 +1,38 @@
+# coding=UTF-8
+import json
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+class Crawler():
+    baseURL = "http://www.tlkg.com/activityweb/activityMvList.kg"
+
+    def request(self, race_id):
+        # print(requests.get(self.baseURL+race_id).url)
+        payload = {'key': 0, 'page': 1, 'actid': race_id}
+        return requests.post(self.baseURL, data=payload).content
+    def parse(self, response):
+        print(json.loads(response)['content'])
+        return json.dumps(json.loads(response)['content'],sort_keys = True ,indent = 4 )
+        # json.dumps(response,sort_keys = True ,indent = 4 )
+        # soup = BeautifulSoup(response, 'html.parser')
+        # yield soup.find_all('div', { "class" : "musicname" })
+        # yield (soup.find_all('div', { "class" : "mvname" }))
+
+crawler = Crawler()
+
+@app.route("/")
+def index():
+    actid = request.args.get('actid')
+    print actid
+    content = crawler.request('381864')
+    result = crawler.parse(content)
+    return (result)
+
+if __name__ == '__main__':
+    app.run()
+    
+    
